f71d41f23dbe9429fc23beffbb84a1b1861e5f49
Qi LI <liqi0425@gmail.com>
2014-09-04 14:19:56 +0200

    build package 'admin'

---
+++ src/admin-src/mklib.sh
@@ -0,0 +1,2 @@
+ocamlfind remove admin
+ocamlfind install admin META _obuild/webedit_admin/admintool.cmi _obuild/webedit_admin/admintool.cmo _obuild/webedit_admin/admintool.o _obuild/webedit_admin/admintool.cmx
